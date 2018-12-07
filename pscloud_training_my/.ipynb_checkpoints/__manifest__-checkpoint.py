c8b0507f457b73f0550893a1819fb8817b425c63
juzsoft2018 <cimage@163.com>
2018-12-07 11:13:34 +0000

    juzsoft2018-4

--- pscloud_training_my/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ pscloud_training_my/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'PS Cloud 培训-测试',
+    'name': 'PS Cloud 培训',
     'version': '12.0.1.0',
     'summary': 'PS Cloud 培训',
     'author': "www.mypscloud.com",
@@ -8,15 +8,19 @@
     'category': 'Tools',
     'depends': ['base'],
     'data': [
+        'security/ir.model.access.csv',
+        'wizard/student_regiester_views.xml',
         'views/res_partner_views.xml',
         'views/training_lesson_views.xml',
         'views/training_subject_views.xml',
         'views/training_views.xml',
     ],
-   
+    'demo': [
+        'demo/pscloud_demo.xml',
+    ],
     'qweb': [],
     'js': [],
     'css': [],
     'auto_install': False,
     'application': True,
-}
+}v
